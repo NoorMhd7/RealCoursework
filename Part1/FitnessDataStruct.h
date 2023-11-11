fcd6465a8aba5ae9c9a5eb860b937a4ab59afbee
NoorMhd7 <146771080+NoorMhd7@users.noreply.github.com>
2023-11-11 21:17:28 +0000

    Task2

---
+++ Part1/FitnessDataStruct.h
@@ -0,0 +1,100 @@
+#ifndef FITNESS_DATA_STRUCT_H
+#define FITNESS_DATA_STRUCT_H
+
+#include <stdio.h>
+
+// Define an appropriate struct
+typedef struct {
+	char date[11];
+	char time[6];
+	int steps;
+} FITNESS_DATA;
+
+// Helper function prototypes
+void tokeniseRecord(const char *input, const char *delimiter, char *date, char *time, char *steps);
+
+// Implementation for finding the index of the record with the fewest steps. 
+int findFewestSteps(const FITNESS_DATA *data, int size) {
+    int minIndex = 0;
+    int minSteps = data[0].steps;
+   //  Source: https://www.youtube.com/watch?v=9DJYGr2W5y8
+    for (int i = 1; i < size; ++i) {
+        if (data[i].steps < minSteps) {
+            minIndex = i;
+            minSteps = data[i].steps;
+        }
+    }
+
+    return minIndex;
+}
+
+// Implementation for finding the index of the record with the largest steps. 
+int findLargestSteps(const FITNESS_DATA *data, int size) {
+    
+    int maxIndex = 0;
+    
+    int maxSteps = data[0].steps;
+
+    // Source: https://www.youtube.com/watch?v=9DJYGr2W5y8
+    for (int i = 1; i < size; ++i) {
+        
+        if (data[i].steps > maxSteps) {
+            maxIndex = i;
+            maxSteps = data[i].steps;
+        }
+    }
+
+    return maxIndex;
+}
+
+int meanSteps(const FITNESS_DATA *data, int size) {
+    if (size == 0) {
+        return 0;  // Avoid division by zero
+    }
+
+    int sum = 0;
+
+    // Iterate through each element in the data array
+    for (int i = 0; i < size; i++) {
+        sum += data[i].steps;
+    }
+
+    // Calculate the mean
+    int mean = sum / size;
+
+    return mean;
+}
+
+int Longestperiod(const FITNESS_DATA *data, int size) {
+    int maxLength = 0;
+    int startIndex = 0;
+    int currentLength = 0;
+    int currentStart = 0;
+
+    for (int i = 0; i < size; i++) {
+        if (data[i].steps > 500) {
+            currentLength++;
+        } else {
+            if (currentLength > maxLength) {
+                maxLength = currentLength;
+                startIndex = currentStart;
+            }
+            currentLength = 0;
+            currentStart = i + 1;
+        }
+    }
+
+    // Check at the end of the array
+    if (currentLength > maxLength) {
+        maxLength = currentLength;
+        startIndex = currentStart;
+    }
+
+    // Output the result
+         printf("Longest continuous period above 500 steps:\nLongest period start: %s %s\nLongest period end: %s %s\n",
+            data[startIndex].date, data[startIndex].time, data[startIndex + maxLength - 1].date, data[startIndex + maxLength - 1].time);
+      
+     
+}     
+        
+#endif // FITNESS_DATA_STRUCT_H
---
+++ Part1/StepCounter_Final.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "FitnessDataStruct.h"
+
+// Struct moved to header file
+
+// Define any additional variables here
+const int MAX_RECORDS = 1000;
+
+// Global variables for filename and FITNESS_DATA array
+
+
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {        strcpy(date, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
+                    }
+
+int main() {
+    // Declare an array to store FITNESS_DATA
+    FITNESS_DATA data[MAX_RECORDS];
+    
+    
+
+    // Open the CSV file for reading
+    char filename[] = "FitnessData_2023.csv";
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("");
+        return 1;
+    }
+
+    // Read data from the file
+    char line[100];
+    int recordCount = 0;
+
+    // while loop which counts and stores values from the file by tokeniseRecord function
+    while (fgets(line, sizeof(line), file)) {
+        // formats each value to its designated string
+        char date[11], time[6], steps[11];
+        tokeniseRecord(line, ",", date, time, steps);
+        // copies data to its destination. date, time and steps
+        strcpy(data[recordCount].date, date);
+        strcpy(data[recordCount].time, time);
+        data[recordCount].steps = atoi(steps);
+
+        recordCount++;
+    }       
+    int minIndex = findFewestSteps(data, recordCount);
+    int maxIndex = findLargestSteps(data, recordCount);
+    int mean = meanSteps(data, recordCount);
+    Longestperiod(data, recordCount);
+     
+     char choice;
+    printf("Menu:\n1. Option A\n2. Option B\n3. Option C\n4. Option D\n5. Option E\n6. Option F\n7. Option Q\nEnter your choice: ");
+    scanf("%c", &choice);
+
+    switch (choice) {
+        case 'A':
+            printf("Input filename: %s\n", filename);
+            break;
+        case 'B':
+            printf("Total records: %d\n", recordCount);
+            break;
+        case 'C':
+            printf("Fewest Steps: %s %s\n", data[minIndex].date, data[minIndex].time);
+            break;
+        case 'D':
+            printf("Largest steps: %s %s\n", data[maxIndex].date, data[maxIndex].time);
+            break;
+        case 'E':
+            printf("Mean step count: %d\n", mean);
+            break;
+        case 'F':
+           Longestperiod(data, recordCount);
+           break;
+        case 'Q':
+            printf("Exiting the program.\n");
+            return 0;
+        
+        
+              
+    }
+
+    return 0;
+}
